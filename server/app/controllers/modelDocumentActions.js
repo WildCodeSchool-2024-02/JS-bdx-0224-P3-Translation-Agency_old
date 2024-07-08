747acb6608fa62476cb38f269cb95d07c6bf69ff
NESRINERABAAOUI <161383698+NESRINERABAAOUI@users.noreply.github.com>
2024-07-08 15:36:11 +0200

    controllers

--- server/app/controllers/modelDocumentActions.js
+++ server/app/controllers/modelDocumentActions.js
@@ -4,11 +4,11 @@ const tables = require("../../database/tables");
 // The B of BREAD - Browse (Read All) operation
 const browse = async (req, res, next) => {
   try {
-    // Fetch all clients from the database
-    const clientObjects = await tables.client.readAll();
+    // Fetch all model document from the database
+    const models = await tables.modelDocs.readAll();
 
-    // Respond with the items in JSON format
-    res.json(clientObjects);
+    // Respond with the model document in JSON format
+    res.json(models);
   } catch (err) {
     // Pass any errors to the error-handling middleware
     next(err);
@@ -18,15 +18,15 @@ const browse = async (req, res, next) => {
 // The R of BREAD - Read operation
 const read = async (req, res, next) => {
   try {
-    // Fetch a specific item from the database based on the provided ID
-    const clientObject = await tables.client.read(req.params.id);
+    // Fetch a specific model document from the database based on the provided ID
+    const modelDoc = await tables.modelDocs.read(req.params.id);
 
     // If the item is not found, respond with HTTP 404 (Not Found)
-    // Otherwise, respond with the client in JSON format
-    if (clientObject == null) {
+    // Otherwise, respond with the model document in JSON format
+    if (modelDoc == null) {
       res.sendStatus(404);
     } else {
-      res.json(clientObject);
+      res.json(modelDoc);
     }
   } catch (err) {
     // Pass any errors to the error-handling middleware
@@ -36,31 +36,30 @@ const read = async (req, res, next) => {
 
 // The A of BREAD - Add (Create) operation
 const add = async (req, res, next) => {
-  // Extract the client data from the request body
-  const clientBody = req.body;
+  // Extract the model document data from the request body
+  const modelDocBody = req.body;
 
   try {
     // Insert the item into the database
-    const insertId = await tables.client.create(clientBody);
+    const insertId = await tables.modelDocs.create(modelDocBody);
 
-  // Respond with HTTP 201 (Created) client
+    // Respond with HTTP 201 (Created) and the ID of the newly inserted model_document
     res.status(201).json({ insertId });
   } catch (err) {
     // Pass any errors to the error-handling middleware
     next(err);
   }
 };
-
-/// The E of BREAD - Edit (Update) operation
+// The E of BREAD - Edit (Update) operation
 const edit = async (req, res, next) => {
-  // Extract the client data from the request body
-  const clientBody = req.body;
+  // Extract the model document data from the request body
+  const modelDocBody = req.body;
 
   try {
-    // Insert the item into the database
-    const modifiedId = await tables.client.update(clientBody);
+    // edit the model document into the database
+    const modifiedId = await tables.modelDocs.update(modelDocBody);
 
-    // Respond with HTTP 200 (updated) client 
+    // Respond with HTTP 200 (updated) model document 
     res.status(200).json({ modifiedId });
   } catch (err) {
     // Pass any errors to the error-handling middleware
@@ -70,14 +69,14 @@ const edit = async (req, res, next) => {
 
 // The D of BREAD - Destroy (Delete) operation
 const destroy = async (req, res, next) => {
-  // Extract the client id from the request params
-  const clientId = req.params.id;
+  // Extract the model document id from the request params
+  const modelDocId = req.params.id;
 
   try {
-    // Insert the client into the database
-    const deletedId = await tables.client.delete(clientId);
+    // delete the model document from the database
+    const deletedId = await tables.translator.delete(modelDocId);
 
-    // Respond with HTTP 204 (deleted) client 
+    // Respond with HTTP 204 (deleted) translator 
     res.status(204).json({ deletedId });
   } catch (err) {
     // Pass any errors to the error-handling middleware
@@ -90,5 +89,5 @@ module.exports = {
   read,
   edit,
   add,
-  destroy,
+  destroy
 };
